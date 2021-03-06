1eb9b57dfe98c24bd414b92e6d12493d1d885fab
Samuel Rocha <samuelsilv.rocha@gmail.com>
2021-03-06 18:20:23 -0300

    add jsons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import { useEffect, useRef, useState } from 'react';
-import { ForceGraph3D, ForceGraph2D } from 'react-force-graph';
 import Force from './Force';
 
 function App() {
   return (
     <div className="App">
-      <Force />
+      <Force graphType={'3D'}/>
     </div>
   );
 }
