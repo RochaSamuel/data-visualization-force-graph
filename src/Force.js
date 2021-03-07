e34a19fcb5b361a1424e7f48a8b6021b24946e1a
Samuel Rocha <samuelsilv.rocha@gmail.com>
2021-03-07 00:43:28 -0300

    Adicionado filtros por assunto

--- src/Force.js
+++ src/Force.js
@@ -1,26 +1,92 @@
 import "./App.css";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { ForceGraph3D, ForceGraph2D } from "react-force-graph";
+import Search from "./components/Search";
+import AssuntoFilter from "./components/AssuntoFilter";
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { FixedSizeList } from 'react-window';
 
 function Force() {
     const graphRef = useRef(null);
     const [nodes, setNodes] = useState([]);
     const [links, setLinks] = useState([]);
 
+    const [secureLinks, setSecureLinks] = useState([])
+    const [secureNodes, setSecureNodes] = useState([])
+
+    const [filteredTemp, setFilteredTemp] = useState([])
+
+    const [dataShouldBeUpdated, setDataShouldBeUpdated] = useState(false);
+    const [assuntoFilter, setAssuntoFilter] = useState([]);
+
+    const [openFilteredResults, setOpenFilteredResults] = useState(false);
+
     useEffect(() => {
         fetch(
-        "https://raw.githubusercontent.com/RochaSamuel/data-visualization-force-graph/master/data/linksv2.json"
+        "https://raw.githubusercontent.com/RochaSamuel/data-visualization-force-graph/master/linksv2.json"
         )
         .then((data) => data.json())
-        .then((json) => setLinks(json));
+        .then((json) => {
+            setLinks(json)
+            setSecureLinks(json)
+        });
 
         fetch(
-        "https://raw.githubusercontent.com/RochaSamuel/data-visualization-force-graph/master/data/nodesv2.json"
+        "https://raw.githubusercontent.com/RochaSamuel/data-visualization-force-graph/master/nodesv2.json"
         )
         .then((data) => data.json())
-        .then((json) => setNodes(json));
+        .then((json) => {
+            setNodes(json)
+            setSecureNodes(json)
+        });
     }, []);
 
+    useEffect(() => {
+        let filteredNodes
+        let filteredLinks
+        if(assuntoFilter.length > 0) {
+            filteredNodes = secureNodes.filter(node => assuntoFilter.includes(node.assunto));
+            filteredLinks = secureLinks.filter(link => [...filteredNodes.map(node => node.id)].includes(link.source.id) && [...filteredNodes.map(node => node.id)].includes(link.target.id))
+        } else {
+            filteredLinks = secureLinks;
+            filteredNodes = secureNodes;
+        }
+        // setNodes(filteredNodes);
+        // setLinks(filteredLinks);
+        setFilteredTemp({nodes: filteredNodes, links: filteredLinks})
+        setOpenFilteredResults(true);
+        setDataShouldBeUpdated(false);
+    }, [dataShouldBeUpdated])
+
+    const handleAssuntoFilter = (value) => {
+        setAssuntoFilter(value)
+        setDataShouldBeUpdated(true);
+    }
+
+
+    const renderFilteredResults = ({nodes, links}) => {
+        const styles = {
+            container: {overflow: 'auto',borderRadius: '10px', background: '#F2F2F2', width: '300px', padding: '15px', height: '400px', position: 'absolute', zIndex: '1', top: '260px', left: '20px'},
+            button: {cursor: 'pointer', background: 'red', color: '#fff', padding: '10px', width: 'fit-content', dsplay: 'flex', alignItems: 'center', justifyContent: 'center', fontWeigth: 'bold', borderRadius: '10px', margin: '20px auto'}
+        }
+
+        return (<>
+            <div style={styles.container}>
+                {nodes.map(node => <div onClick={() => handleNodeClick(node)} style={{cursor: 'pointer', marginBottom: '6px'}}>{node.id}</div>)}
+                <div style={styles.button} onClick={() => {
+                    setNodes(nodes);
+                    setLinks(links);
+                }}>APLICAR FILTROS</div>
+                <div style={styles.button} onClick={() => {
+                    setNodes(secureNodes);
+                    setLinks(secureLinks);
+                }}>REVERTER</div>
+            </div>
+            </>
+        );
+    }
+    
     const handleNodeClick = (node) => {
         const distance = 40;
         const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
@@ -38,15 +104,18 @@ function Force() {
 
     return (
         <div className="Graph">
-        <ForceGraph3D
-            ref={graphRef}
-            graphData={{ nodes: nodes, links: links }}
-            nodeLabel={"id"}
-            linkLabel={(link) => `${link.protocolo}`}
-            nodeAutoColorBy={"assunto"}
-            linkDirectionalArrowLength={3}
-            onNodeClick={ handleNodeClick }
-        />
+            <ForceGraph3D
+                ref={graphRef}
+                graphData={{ nodes: nodes, links: links }}
+                nodeLabel={"id"}
+                linkLabel={(link) => `${link.protocolo}`}
+                nodeAutoColorBy={"assunto"}
+                linkDirectionalArrowLength={3}
+                onNodeClick={ handleNodeClick }
+            />
+            {openFilteredResults && renderFilteredResults(filteredTemp)}
+            <AssuntoFilter assuntos={[...new Set(secureNodes.map(node => node.assunto))]} applyFilter={handleAssuntoFilter}/>
+            {graphRef.current && <Search nodes={nodes} graphRef={graphRef}/>}
         </div>
     );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+const styles = {
+    container: {borderRadius: '10px', background: '#F2F2F2', width: 'fit-content', padding: '15px', height: 'fit-content', position: 'absolute', zIndex: '1', top: '20px', left: '20px'},
+}
 
 export default function Seacrh(props) {
+    console.log(props.nodes)
+    return <div style={styles.container}>
+        <Autocomplete
+            id="combo-box-demo"
+            options={props.nodes}
+            getOptionLabel={(option) => option.id}
+            onInputChange={(event, newInputValue) => {
+                const node = props.nodes.filter((node) => node.id === newInputValue)
 
+                const distance = 40;
+                const distRatio = 1 + distance/Math.hypot(node[0].x, node[0].y, node[0].z);
+                
+                props.graphRef.current.cameraPosition(
+                    { x: node[0].x * distRatio, y: node[0].y * distRatio, z: node[0].z * distRatio }, // new position
+                    node[0], // lookAt ({ x, y, z })
+                    3000  // ms transition duration
+                );
+            }}
+            style={{ width: 300 }}
+            renderInput={(params) => <TextField {...params} label="ID da base" variant="outlined" />}
+        />
+    </div>
 }   
